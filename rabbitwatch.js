f2fdbd2b0d841e6068c18d9b95d22dfe88961e48
talkingscott <talkingscott@gmail.com>
2015-09-17 22:21:06 -0400

    Makes RabbitMQ API calls based on a hard-coded configuration.  Doesn't use the call response data to create the response to its own API calls.  But there's not much work left to get to that point.

--- rabbitwatch.js
+++ rabbitwatch.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var http = require('http');
 
 app.use(morgan('combined'));
 app.use(express.static(__dirname + '/public'));
 
-var all_groups = [];
+var group_configs = [
+  {group_name: 'Group One', host: 'localhost', port: 15672, vhost: '/', user: 'guest', pass: 'guest', queue_names: ['Queue One', 'Queue Two', 'Queue Three']},
+  {group_name: 'Group Two', host: 'localhost', port: 15672, vhost: '/', user: 'guest', pass: 'guest', queue_names: ['Queue One', 'Queue Two', 'Queue Three']}
+];
+
+var group_data = {};
 
 app.get('/api/queues', function (req, res) {
   res.set('Pragma', 'no-cache');
   res.set('Cache-Control', 'no-cache');
-  res.send({groups: all_groups});
+  response = [];
+  group_configs.forEach(function (group_config) {
+    response.push(group_data[group_config.group_name]);
+  });
+  res.send({response: response});
 });
 
 var port = parseInt(process.env['PORT']) || 3000
@@ -19,21 +29,43 @@ var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('rabbitwatch listening at http://%s:%s', host, port);
 });
 
-function getGroups() {
-  var groups = [];
-  ['Group One', 'Group Two', 'Group Three'].forEach(function (group_name) {
-    var queues = [];
-    ['Queue One', 'Queue Two', 'Queue Three'].forEach(function (queue_name) {
-      var d = new Date();
-      queues.push({name: queue_name, state: 'idle', ready: d.getTime() / 1000});
+function getQueueData(host, port, vhost, user, pass, queue_names, cb) {
+  options = {hostname: host, port: port, path: '/api/queues/' + encodeURIComponent(vhost), auth: user+':'+pass, headers: { 'Accept': 'application/json' }}
+  http.get(options, function (res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    var response = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+      response += chunk;
     });
-    groups.push({name: group_name, queues: queues});
+    res.on('end', function() {
+      console.log('No more data in response.')
+      var raw_data = JSON.parse(response)
+      var queue_data = [];
+      queue_names.forEach(function (queue_name) {
+        var d = new Date();
+        queue_data.push({name: queue_name, state: 'idle', ready: d.getTime() / 1000});
+      });
+      cb(queue_data);
+    });
+  }).on('error', function(e) {
+    console.log('ERROR for request to ' + host + ': ' + e.message);
+    cb([]);
   });
-  all_groups = groups;
-  setTimeout(getGroups, 5000);
+}
+
+function getGroupData(group_config) {
+  getQueueData(group_config.host, group_config.port, group_config.vhost, group_config.user, group_config.pass, group_config.queue_names, function (queue_data) {
+    group_data[group_config.group_name] = {name: group_config.group_name, queues: queue_data};
+    setTimeout(getGroupData, 5000, group_config);
+  });
 };
 
-getGroups();
+group_configs.forEach(function (group_config) {
+  getGroupData(group_config);
+});
