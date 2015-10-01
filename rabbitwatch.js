4494ef6f0dec7e91be76857a9390327d05993133
talkingscott <talkingscott@gmail.com>
2015-10-01 04:18:48 -0400

    Make jslint happier with server code.  Well, node-jslint version 0.9.3 2013-08-26, at least.

--- rabbitwatch.js
+++ rabbitwatch.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+/*jslint node: true indent: 2 */
+"use strict";
 
 // node modules
 var fs = require('fs');
@@ -22,7 +24,7 @@ var group_data = {};
 app.get('/api/queues', function (req, res) {
   res.set('Pragma', 'no-cache');
   res.set('Cache-Control', 'no-cache');
-  response = [];
+  var response = [];
   group_configs.forEach(function (group_config) {
     response.push(group_data[group_config.group_name]);
   });
@@ -31,7 +33,7 @@ app.get('/api/queues', function (req, res) {
 
 // Gets data about queues from one server using the RabbitMQ REST API
 function getQueueData(host, port, vhost, user, pass, queue_names, cb) {
-  options = {hostname: host, port: port, path: '/api/queues/' + encodeURIComponent(vhost), auth: user+':'+pass, headers: { 'Accept': 'application/json' }}
+  var options = {hostname: host, port: port, path: '/api/queues/' + encodeURIComponent(vhost), auth: user + ':' + pass, headers: { 'Accept': 'application/json' }};
   http.get(options, function (res) {
     //console.log('STATUS: ' + res.statusCode);
     //console.log('HEADERS: ' + JSON.stringify(res.headers));
@@ -41,30 +43,30 @@ function getQueueData(host, port, vhost, user, pass, queue_names, cb) {
       //console.log('BODY: ' + chunk);
       response += chunk;
     });
-    res.on('end', function() {
-      //console.log('No more data in response.')
-      var raw_data = JSON.parse(response)
-      var queue_data = [];
+    res.on('end', function () {
+      //console.log('No more data in response.');
+      var raw_data = JSON.parse(response),
+        queue_data = [];
       raw_data.forEach(function (data) {
         //console.log('Q: ' + JSON.stringify(data));
         queue_names.forEach(function (queue_name) {
-          var publish_rate;
-          var deliver_rate;
-          var ack_rate;
-          if (data['name'] == queue_name) {
-            if (data['message_stats']) {
-              if (data['message_stats']['publish_details']) {
-                publish_rate = data['message_stats']['publish_details']['rate']
+          var publish_rate,
+            deliver_rate,
+            ack_rate;
+          if (data.name === queue_name) {
+            if (data.hasOwnProperty('message_stats')) {
+              if (data.message_stats.hasOwnProperty('publish_details')) {
+                publish_rate = data.message_stats.publish_details.rate;
               } else {
                 publish_rate = 0.0;
               }
-              if (data['message_stats']['deliver_details']) {
-                deliver_rate = data['message_stats']['deliver_details']['rate']
+              if (data.message_stats.hasOwnProperty('deliver_details')) {
+                deliver_rate = data.message_stats.deliver_details.rate;
               } else {
                 deliver_rate = 0.0;
               }
-              if (data['message_stats']['ack_details']) {
-                ack_rate = data['message_stats']['ack_details']['rate']
+              if (data.message_stats.hasOwnProperty('ack_details')) {
+                ack_rate = data.message_stats.ack_details.rate;
               } else {
                 ack_rate = 0.0;
               }
@@ -73,13 +75,13 @@ function getQueueData(host, port, vhost, user, pass, queue_names, cb) {
               deliver_rate = 0.0;
               ack_rate = 0.0;
             }
-            queue_data.push({name: queue_name, state: data['state'], total: data['messages'], ready: data['messages_ready'], unacknowledged: data['messages_unacknowledged'], publish_rate: publish_rate, deliver_rate: deliver_rate, ack_rate: ack_rate});
+            queue_data.push({name: queue_name, state: data.state, total: data.messages, ready: data.messages_ready, unacknowledged: data.messages_unacknowledged, publish_rate: publish_rate, deliver_rate: deliver_rate, ack_rate: ack_rate});
           }
         });
       });
       cb(queue_data);
     });
-  }).on('error', function(e) {
+  }).on('error', function (e) {
     console.log('ERROR for request to ' + host + ': ' + e.message);
     cb([]);
   });
@@ -88,16 +90,16 @@ function getQueueData(host, port, vhost, user, pass, queue_names, cb) {
 // Updates the data about queues for one group
 // N.B. This schedules itself to run again in 5 seconds
 function getGroupData(group_config) {
-  var group_name = group_config.group_name;
-  var rabbitmq = group_config.rabbitmq;
+  var group_name = group_config.group_name,
+    rabbitmq = group_config.rabbitmq;
   getQueueData(rabbitmq.host, rabbitmq.port, rabbitmq.vhost, rabbitmq.user, rabbitmq.pass, rabbitmq.queue_names, function (queue_data) {
     group_data[group_name] = {name: group_name, rabbitmq: {queues: queue_data}};
     setTimeout(getGroupData, 5000, group_config);
   });
-};
+}
 
 // Start actually doing something by reading the group configuration file
-var group_configs_filename = process.env['GROUP_CONFIGS'] || path.join(__dirname, 'group_configs.json');
+var group_configs_filename =  process.env.hasOwnProperty('GROUP_CONFIGS') ? process.env.GROUP_CONFIGS : path.join(__dirname, 'group_configs.json');
 console.log('Read group configuration from ' + group_configs_filename);
 fs.readFile(group_configs_filename, function (err, data) {
   if (err) {
@@ -109,14 +111,13 @@ fs.readFile(group_configs_filename, function (err, data) {
       getGroupData(group_config);
     });
 
-    var port = parseInt(process.env['PORT']) || 3000
-    
-    var server = app.listen(port, function () {
-      var host = server.address().address;
-      var port = server.address().port;
-    
-      console.log('rabbitwatch listening at http://%s:%s', host, port);
-    });
+    var port = process.env.hasOwnProperty('PORT') ? parseInt(process.env.PORT, 10) : 3000,
+      server = app.listen(port, function () {
+        var server_host = server.address().address,
+          server_port = server.address().port;
+
+        console.log('rabbitwatch listening at http://%s:%s', server_host, server_port);
+      });
   }
 });
 
